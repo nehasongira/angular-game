eadcaa71aa9fca4c4f7f57adfaea9341f13faf1a
nehasongira <songiraneha11@gmail.com>
2019-10-30 17:41:37 +0530

    startendtimer

--- app/src/app/component/component.component.ts
+++ app/src/app/component/component.component.ts
@@ -11,44 +11,98 @@ export class ComponentComponent implements OnInit {
   public value2 = true;
   public value3 = true;
   public value4 = true;
+  timeLeft: number = 10;
+  interval;
   // game(){
+    
+     
    
+   startgame()
+   {
+   
+    setInterval(() => {
+      if(this.timeLeft > 0) {
+        this.timeLeft--;
+      }
+       if(this.timeLeft==0){
+         clearInterval(this.interval);
+
+        //  this.timeLeft=0;
+        window.alert('time is up');
+        
+      }
+     
+    },1000)
+
+    this.interval=setInterval(()=>{
+     
+      var x= Math.floor(Math.random()*4)+1;
+   switch (x) {
+     case 1:
+       this.value1= true;
+       this.value2= false;
+       this.value3= false;
+       this.value4= false;
+       break;
+     case 2:
+       this.value1= false;
+       this.value2= true;
+       this.value3= false;
+       this.value4= false;
+       break;
+     case 3:
+       this.value1= false;
+       this.value2= false;
+       this.value3= true;
+       this.value4= false;
+       break;
+     case 4:
+       this.value1= false;
+       this.value2= false;
+       this.value3= false;
+       this.value4= true;
+       break;
+     default:
+         break;
+   }
+   },1000);
+   }
     
   // }
   constructor() { }
   ngOnInit() {
-    setInterval(()=>{
+    // setInterval(()=>{
      
-       var x= Math.floor(Math.random()*4)+1;
-    switch (x) {
-      case 1:
-        this.value1= true;
-        this.value2= false;
-        this.value3= false;
-        this.value4= false;
-        break;
-      case 2:
-        this.value1= false;
-        this.value2= true;
-        this.value3= false;
-        this.value4= false;
-        break;
-      case 3:
-        this.value1= false;
-        this.value2= false;
-        this.value3= true;
-        this.value4= false;
-        break;
-      case 4:
-        this.value1= false;
-        this.value2= false;
-        this.value3= false;
-        this.value4= true;
-        break;
-      default:
-          break;
-    }
-    },1000);
+    //    var x= Math.floor(Math.random()*4)+1;
+    // switch (x) {
+    //   case 1:
+    //     this.value1= true;
+    //     this.value2= false;
+    //     this.value3= false;
+    //     this.value4= false;
+    //     break;
+    //   case 2:
+    //     this.value1= false;
+    //     this.value2= true;
+    //     this.value3= false;
+    //     this.value4= false;
+    //     break;
+    //   case 3:
+    //     this.value1= false;
+    //     this.value2= false;
+    //     this.value3= true;
+    //     this.value4= false;
+    //     break;
+    //   case 4:
+    //     this.value1= false;
+    //     this.value2= false;
+    //     this.value3= false;
+    //     this.value4= true;
+    //     break;
+    //   default:
+    //       break;
+    // }
+    // },1000);
    
   }
 
